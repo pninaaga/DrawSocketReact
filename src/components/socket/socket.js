76f6aeb15131336ae30c863c7c56339d683e9121
unknown <pnina@leader.codes>
2021-10-12 03:42:04 +0300

    fixed draw

--- src/components/socket/socket.js
+++ src/components/socket/socket.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-// import { useLocation } from 'react-router-dom'
 import socketIoClient from 'socket.io-client'
 import PenDraw from '../draw/draw'
 import './socket.css'
@@ -10,25 +9,9 @@ const ENDPOINT = 'http://localhost:3000/'
 export default function Socket() {
 
     const socket = socketIoClient(ENDPOINT)
-    // const location = useLocation()
-    // const { startMessage, name } = location.state
-    // const [messageText, setMessegeText] = useState('')
-    // const [resMessage, setResMessage] = useState()
-
-    useEffect(() => {
-        // setResMessage(startMessage)
-        // socket.on('messageToClient', (resMessage) => {
-        //     setResMessage(resMessage)
-        // })
-    }, [])
-
-    // function sendMessage() {
-    //     socket.emit('sendMessage', messageText)
-    // }
 
     return (
         <>
-            {/* <label className="title-draw">Drawing</label> */}
             <PenDraw socket={socket}></PenDraw>
         </>
     )
